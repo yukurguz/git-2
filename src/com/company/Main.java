2a4089fddd8f2c1aa17e0a002e81be86fb454c6f
yukurguz <30299790+yukurguz@users.noreply.github.com>
2018-06-09 01:52:15 +0300

    TEST 3
    Signed-off-by: yukurguz <30299790+yukurguz@users.noreply.github.com>

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,21 +1,18 @@
 package com.company;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class Main {
-    public static void main(String[] args) throws IOException
+    public static void main(String[] args)
     {
-        ArrayList<Integer> list1 = new ArrayList<Integer>();   //создание списка
+        ArrayList<Integer> list1 = new ArrayList<>();   //создание списка
         Collections.addAll(list1, 1, 5, 6, 11, 3, 15, 7, 8);   //заполнение списка
 
-        ArrayList<Integer> list2 = new ArrayList<Integer>();
-        Collections.addAll(list2, 1, 8, 6, 21, 53, 5, 67, 18);
+        ArrayList<Integer> list2 = new ArrayList<>();
+        Collections.addAll(list2, 1, 8, 6, 21, 53, 5, 57, 18);
 
-        ArrayList<Integer> result = new ArrayList<Integer>();
+        ArrayList<Integer> result = new ArrayList<>();
 
         result.addAll(list1);   //добавление всех значений из одного списка в другой
         result.addAll(list2);
